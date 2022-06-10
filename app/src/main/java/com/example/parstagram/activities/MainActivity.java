49303328ed97eeb9cbdfd9a9c3c67ede33d5f745
Whitney Deng <whitneyd@fb.com>
2022-06-09 22:48:16 -0700

    Added logout to action bar

--- app/src/main/java/com/example/parstagram/activities/MainActivity.java
+++ app/src/main/java/com/example/parstagram/activities/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.parstagram.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.parstagram.R;
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -14,5 +19,36 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // Menu icons are inflated just as they were with actionbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch(id)
+        {
+            case R.id.miLogout:
+                logout();
+            default:
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout()
+    {
+        ParseUser.logOut();
+//        ParseUser currentUser = ParseUser.getCurrentUser();
     }
 }
