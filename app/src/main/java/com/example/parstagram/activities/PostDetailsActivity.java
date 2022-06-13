a6a9c7500e7fa3000b2c6b9fe80f30a58b135aa4
WhitneyDeng <32890361+WhitneyDeng@users.noreply.github.com>
2022-06-13 04:53:15 -0700

    Step 6c Post Detail View in progress: added empty Activity class & xml + setup onclick in PostsAdapter

---
+++ app/src/main/java/com/example/parstagram/activities/PostDetailsActivity.java
@@ -0,0 +1,18 @@
+package com.example.parstagram.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.parstagram.R;
+
+public class PostDetailsActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_details);
+    }
+}
--- app/src/main/java/com/example/parstagram/adapters/PostsAdapter.java
+++ app/src/main/java/com/example/parstagram/adapters/PostsAdapter.java
@@ -1,6 +1,7 @@
 package com.example.parstagram.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.example.parstagram.R;
+import com.example.parstagram.activities.PostDetailsActivity;
 import com.example.parstagram.model.Post;
 import com.parse.ParseFile;
 
@@ -64,7 +66,7 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder>
         notifyDataSetChanged();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
     {
         private TextView tvUsername;
         private ImageView ivImage;
@@ -95,6 +97,27 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder>
             {
                 // empty ImageView if post has no image
                 ivImage.setImageResource(0);
+            }
+        }
+
+        // if row is clicked
+        @Override
+        public void onClick(View v)
+        {
+            // gets item position
+            int position = getAdapterPosition();
+            // make sure the position is valid, i.e. actually exists in the view
+            if (position != RecyclerView.NO_POSITION) {
+                // get the post at the position, this won't work if the class is static
+                Post post = posts.get(position);
+                // create intent for the new activity
+                Intent intent = new Intent(context, PostDetailsActivity.class);
+                /* serialize the movie using parceler, use its short name as a key
+                 * imow: wrap up Movie object to be delivered to the MovieDetailsActivity
+                 * .class.getSimpleName(): https://www.geeksforgeeks.org/class-getsimplename-method-in-java-with-examples/*/
+                intent.putExtra(Post.class.getSimpleName(), Parcels.wrap(post));
+                // show the activity
+                context.startActivity(intent);
             }
         }
     }
